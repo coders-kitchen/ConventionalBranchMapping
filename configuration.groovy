3d5ed32f271f3ce8cd0aa0af16989dac3307441a
Peter Daum <peter.daum.80@gmail.com>
2013-11-02 19:39:11 +0100

    Initial import

---
+++ configuration.groovy
@@ -0,0 +1,12 @@
+message = "I'm the default"
+environment {
+	production {
+		message = "I am the producing machine"
+	}
+	staging {
+		message = "Yo man! You are on the stage again"
+	}
+	test {
+		message = "Testing? Trust me works great!"
+	}
+}
